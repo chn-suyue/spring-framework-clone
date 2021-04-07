ef277b6deae623d23d22cde01081225b8fd73b0e
suuuuuuuuuuu <294490452@qq.com>
2021-04-07 13:54:18 +0800

    spring

---
+++ my-test/src/main/java/MyApplication.java
@@ -0,0 +1,19 @@
+/**
+ * @description:
+ * @author: suyue
+ * @time: 2021/4/7 11:51
+ */
+
+import bean.Hello;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+		Hello hello = (Hello)ac.getBean("hello");
+		hello.sayHello();
+	}
+
+}
---
+++ my-test/src/main/java/bean/Hello.java
@@ -0,0 +1,14 @@
+package bean;
+
+/**
+ * @description:
+ * @author: suyue
+ * @time: 2021/4/7 11:53
+ */
+public class Hello {
+
+	public void sayHello() {
+		System.out.println("Hello world");
+	}
+
+}
